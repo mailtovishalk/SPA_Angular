1401d25967e3ae4363fadad90276806dc5595759
mailtovishalk <34046831+mailtovishalk@users.noreply.github.com>
2019-07-22 21:52:04 +1000

    src added

---
+++ src/app/products/product.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { IProduct } from "./product";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError,tap } from 'rxjs/operators'
+@Injectable({
+    providedIn:'root'
+})
+export class ProductService{
+    private productUrl = 'api/products/products.json';
+
+    constructor(private http:HttpClient){}
+
+getProducts(): Observable<IProduct[]>{
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+        tap(data=> console.log('All:'+JSON.stringify(data))),catchError(this.handleError)
+    );
+}
+   private handleError(err:HttpErrorResponse){
+       let errMessage = '';
+       if(err.error instanceof ErrorEvent)
+       {
+           errMessage = `An error occured:${err.error.message}`;
+       }
+       else
+       {
+        errMessage = `Server returned code :${err.status},error message is:${err.message}`; 
+       }
+       console.error(errMessage);
+       return throwError(errMessage);
+   }
+}
